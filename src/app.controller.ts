413267710b4b46e0ddad343a653d72b81ef28734
Jonathan Russ, GHR-OSA-NEX-33 <jonathan.russ@swisscom.com>
2023-02-15 14:40:21 +0100

    chore(services): update book service & add prisma service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,47 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book as BookModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly bookService: BookService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('books')
+  async getAllBooks(): Promise<BookModel[]> {
+    return this.bookService.books({});
+  }
+
+  @Get('books/:id')
+  async getBook(@Param('id') id: string): Promise<BookModel> {
+    return this.bookService.book({ id: Number(id) });
+  }
+
+  @Post('books')
+  async createBook(@Body() data: any): Promise<BookModel> {
+    return this.bookService.createBook(data);
+  }
+
+  @Put('books/:id')
+  async updateBook(
+    @Param('id') id: string,
+    @Body() data: any,
+  ): Promise<BookModel> {
+    return this.bookService.updateBook({
+      where: { id: Number(id) },
+      data,
+    });
+  }
+
+  @Delete('books/:id')
+  async deleteBook(@Param('id') id: string): Promise<BookModel> {
+    return this.bookService.deleteBook({ id: Number(id) });
   }
 }
