f9d5588cde88af13a594acb02cb9dd83dca676d2
Jonathan Russ, GHR-POD-NEX-33 <jonathan.russ@swisscom.com>
2023-02-21 15:00:23 +0100

    chore(schema.prisma): update schema.prisma

--- prisma/migrations/20230221135309_init/migration.sql
+++ prisma/migrations/20230221135309_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE `Book` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(36) NOT NULL,
     `title` VARCHAR(100) NOT NULL,
     `releaseYear` YEAR NOT NULL,
     `author` VARCHAR(30) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE `Book` (
 
 -- CreateTable
 CREATE TABLE `User` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(36) NOT NULL,
     `username` VARCHAR(30) NOT NULL,
     `password` VARCHAR(100) NOT NULL,
     `firstname` VARCHAR(30) NOT NULL,
@@ -24,25 +24,25 @@ CREATE TABLE `User` (
 
 -- CreateTable
 CREATE TABLE `Note` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(36) NOT NULL,
     `title` VARCHAR(100) NOT NULL,
     `content` TEXT NOT NULL,
-    `noteCollectionId` INTEGER NULL,
+    `noteCollectionId` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `NoteCollection` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(36) NOT NULL,
     `title` VARCHAR(100) NOT NULL,
-    `userId` INTEGER NOT NULL,
+    `userId` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Note` ADD CONSTRAINT `Note_noteCollectionId_fkey` FOREIGN KEY (`noteCollectionId`) REFERENCES `NoteCollection`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Note` ADD CONSTRAINT `Note_noteCollectionId_fkey` FOREIGN KEY (`noteCollectionId`) REFERENCES `NoteCollection`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `NoteCollection` ADD CONSTRAINT `NoteCollection_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `NoteCollection` ADD CONSTRAINT `NoteCollection_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
