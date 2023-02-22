cbea673c26056b10117f7f681314205788f9e980
Jonathan Russ, GHR-POD-NEX-33 <jonathan.russ@swisscom.com>
2023-02-22 09:07:50 +0100

    chore(dependencies): remove passport-local dependency

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,4 +1,4 @@
-import { Strategy } from 'passport-local';
+import { Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
