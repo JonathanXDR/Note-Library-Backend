bb42d3b95d5e4e1cbce619fda32e7432e62d3627
Jonathan Russ, GHR-POD-NEX-33 <jonathan.russ@swisscom.com>
2023-02-28 14:27:31 +0100

    chore(database export): update mariadb.sql

--- database/exports/mariadb.sql
+++ database/exports/mariadb.sql
@@ -4,7 +4,7 @@
 -- https://tableplus.com/
 --
 -- Database: mariadb
--- Generation Time: 2023-02-28 09:09:58.3300
+-- Generation Time: 2023-02-28 14:22:26.6730
 -- -------------------------------------------------------------
 
 
@@ -53,18 +53,23 @@ CREATE TABLE `User` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `Note` (`id`, `title`, `content`, `noteCollectionId`) VALUES
-('03870b8d-3b3a-5f3e-a299-6b0509902d30', 'Grocery List', 'Eggs\nMilk\nBread\nBananas\nChicken breasts\nBroccoli\nCarrots\nPotatoes\nCheese', 'b9c13633-8e11-59e2-bb62-97f47f774b8e'),
-('1d0cd0bc-78c4-5175-b6ba-8581f6de599a', 'Brainstorming Session', 'Generated ideas for new product features\nDiscussed customer feedback and pain points\nPrioritized features based on impact and feasibility\nAssigned tasks to team members for next steps\nScheduled follow-up meeting', 'b9c13633-8e11-59e2-bb62-97f47f774b8e'),
-('57cac3d2-190e-5402-8c0a-c3013e526a1e', 'Meeting Notes', 'Discussed progress on current project\nIdentified roadblocks and potential solutions\nAgreed on timeline for next steps\nAssigned tasks to team members\nScheduled next meeting', 'b9c13633-8e11-59e2-bb62-97f47f774b8e'),
-('b291ceb3-0c11-5171-90fc-9209b3df9dbe', 'Recipe', 'Ingredients:\n1 lb ground beef\n1 onion, chopped\n2 cloves garlic, minced\n1 can diced tomatoes\n1 can kidney beans\n1 tbsp chili powder\nSalt and pepper to taste\nDirections:\nBrown the ground beef in a large pot over medium-high heat.\nAdd the onion and garlic and cook until softened.\nAdd the diced tomatoes, kidney beans, chili powder, salt, and pepper.\nSimmer for 20 minutes, stirring occasionally.', 'e3e59734-2d21-5a09-a126-d4851f0a97dc'),
-('ef4fc691-c1f1-5cf3-9ff4-f656c061b988', 'Travel Itinerary', 'Flight to Tokyo on May 5th\n3 nights in Tokyo\nTrain to Kyoto on May 8th\n4 nights in Kyoto\nTrain to Hiroshima on May 12th\n2 nights in Hiroshima\nFlight back on May 14th', '414cd979-8237-51c9-a362-fc6f20ce2657');
+('2b1478e3-3a24-5c32-bca3-0d311fa7f89b', 'Non-Fiction', 'The Power of Habit by Charles Duhigg, Thinking, Fast and Slow by Daniel Kahneman, and Sapiens: A Brief History of Humankind by Yuval Noah Harari', 'a53d93f8-084c-5b90-9262-cd14d8a1df95'),
+('3c3c3bdf-d7c8-51c7-957c-cc1e8730cf35', 'Ingredients for dinner', 'Chicken, potatoes, broccoli, and carrots', '859f60d8-7c02-5c0d-9b9a-dc764d1f39cc'),
+('3e9e771a-9f9d-5e77-9a80-bec24f5c5b6e', 'Mobile App Development', 'Create a fitness tracking app for iOS and Android', '47fbf5bb-b35a-5d57-b6b8-2b4db9f80fae'),
+('45d413b1-1b7a-54d1-8dfc-6ba065ecb234', 'Web Development', 'Build an e-commerce site using React and Node.js', '47fbf5bb-b35a-5d57-b6b8-2b4db9f80fae'),
+('4a7b34eb-6e22-57d1-9652-ffeb5c61d5a5', 'Snacks for the party', 'Chips, salsa, guacamole, cheese and crackers', '859f60d8-7c02-5c0d-9b9a-dc764d1f39cc'),
+('5035d5a5-5c5b-52f8-83c7-9d316b0fa7e3', 'Mountain Vacation', 'Colorado, Switzerland, or New Zealand', '1359b3e3-4d8f-55a4-b87f-2d429c326944'),
+('5a15e1a8-83e3-5c88-85a4-4d9a70a74a7a', 'Cardio Day', '20 minutes of running, 30 minutes of cycling, and 10 minutes of jumping jacks', '31b6e17b-0779-5de1-8d8c-b08c7496db5f'),
+('7f78e281-0b80-5b90-9d14-c5d5be48cf0c', 'Strength Training Day', '3 sets of bench press, 3 sets of deadlifts, and 3 sets of squats', '31b6e17b-0779-5de1-8d8c-b08c7496db5f'),
+('b5560f91-df70-5c10-81f6-63c96f2087de', 'Fiction', 'The Great Gatsby by F. Scott Fitzgerald, To Kill a Mockingbird by Harper Lee, and 1984 by George Orwell', 'a53d93f8-084c-5b90-9262-cd14d8a1df95'),
+('e10fa89d-c18a-58c9-9f0b-bfaa28a8d1c7', 'Beach Vacation', 'Florida, Hawaii, or Caribbean Islands', '1359b3e3-4d8f-55a4-b87f-2d429c326944');
 
 INSERT INTO `NoteCollection` (`id`, `title`, `userId`) VALUES
-('25d10876-8f26-5369-a8fb-b73c34dd9c80', 'Exploring the World', 'f15e9d13-2766-570a-8d3e-3d766cbcf547'),
-('414cd979-8237-51c9-a362-fc6f20ce2657', 'A Journey Through the Mind', 'f15e9d13-2766-570a-8d3e-3d766cbcf547'),
-('4de794f8-e9b2-5025-ab64-f9b38bc193a1', 'Life Lessons Learned', 'f15e9d13-2766-570a-8d3e-3d766cbcf547'),
-('b9c13633-8e11-59e2-bb62-97f47f774b8e', 'Musings and Reflections', 'e61d63b0-7824-5d39-8837-a4018b1ca868'),
-('e3e59734-2d21-5a09-a126-d4851f0a97dc', 'From Ideas to Action', 'e61d63b0-7824-5d39-8837-a4018b1ca868');
+('1359b3e3-4d8f-55a4-b87f-2d429c326944', 'Vacation Ideas', 'f15e9d13-2766-570a-8d3e-3d766cbcf547'),
+('31b6e17b-0779-5de1-8d8c-b08c7496db5f', 'Workout Plan', 'f15e9d13-2766-570a-8d3e-3d766cbcf547'),
+('47fbf5bb-b35a-5d57-b6b8-2b4db9f80fae', 'Project Ideas', 'f15e9d13-2766-570a-8d3e-3d766cbcf547'),
+('859f60d8-7c02-5c0d-9b9a-dc764d1f39cc', 'Grocery List', 'e61d63b0-7824-5d39-8837-a4018b1ca868'),
+('a53d93f8-084c-5b90-9262-cd14d8a1df95', 'Books to Read', 'e61d63b0-7824-5d39-8837-a4018b1ca868');
 
 INSERT INTO `User` (`id`, `username`, `password`, `firstname`, `lastname`, `age`, `gender`) VALUES
 ('e61d63b0-7824-5d39-8837-a4018b1ca868', 'username', '$2a$10$GuvQCEzYPC4rsht7OriHPuohNy4wlNopatcizt6Yqf8PWkg58MpGq', 'username', 'password', 99, 'Credentials'),
