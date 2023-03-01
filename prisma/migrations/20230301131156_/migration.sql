a06830cf6eae467eba7f9a6ab7414f21ad2afa40
Jonathan Russ, GHR-POD-NEX-33 <jonathan.russ@swisscom.com>
2023-03-01 14:12:21 +0100

    chore(migration): update migration.sql

--- prisma/migrations/20230301131156_/migration.sql
+++ prisma/migrations/20230301131156_/migration.sql
@@ -19,7 +19,7 @@ CREATE TABLE `Note` (
     `id` VARCHAR(36) NOT NULL,
     `title` VARCHAR(100) NULL,
     `content` TEXT NOT NULL,
-    `userId` VARCHAR(36) NULL,
+    `userId` VARCHAR(36) NOT NULL,
     `noteCollectionId` VARCHAR(36) NULL,
 
     PRIMARY KEY (`id`)
@@ -29,16 +29,16 @@ CREATE TABLE `Note` (
 CREATE TABLE `NoteCollection` (
     `id` VARCHAR(36) NOT NULL,
     `title` VARCHAR(100) NOT NULL,
-    `userId` VARCHAR(36) NULL,
+    `userId` VARCHAR(36) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Note` ADD CONSTRAINT `Note_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Note` ADD CONSTRAINT `Note_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Note` ADD CONSTRAINT `Note_noteCollectionId_fkey` FOREIGN KEY (`noteCollectionId`) REFERENCES `NoteCollection`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `NoteCollection` ADD CONSTRAINT `NoteCollection_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `NoteCollection` ADD CONSTRAINT `NoteCollection_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
