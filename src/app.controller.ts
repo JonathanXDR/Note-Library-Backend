e404624d29839995346b9e4639003ab4260a328a
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-04-26 13:31:09 +0200

    feat(service): add app service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { ApiTags } from '@nestjs/swagger';
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly appService: AppService,
+  ) {}
+
+  @Get()
+  @ApiTags('default')
+  getWelcome(): string {
+    return this.appService.getWelcome();
+  }
 
   @UseGuards(LocalAuthGuard)
   @Post('/auth/login')
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AuthModule } from './auth/auth.module';
 import { NoteCollectionsModule } from './noteCollection/noteCollections.module';
 import { NotesModule } from './notes/notes.module';
 import { UsersModule } from './users/users.module';
+import { AppService } from './app.service';
 
 @Module({
   imports: [UsersModule, AuthModule, NotesModule, NoteCollectionsModule],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
